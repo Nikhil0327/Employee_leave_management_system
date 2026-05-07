0a4fcd5a4f385e02636be81b66ac0117b80255eb
Nikhil0327 <vemulanikhil27@gmail.com>
2026-05-07 21:58:26 +0530

    Update API endpoint for registration metadata

--- frontend/src/api/meta.js
+++ frontend/src/api/meta.js
@@ -1,6 +1,6 @@
 import api from "./client.js";
 
 export async function getRegistrationMeta() {
-  const { data } = await api.get("/public/registration-meta");
+  const { data } = await api.get("/api/public/registration-meta");
   return data;
 }
