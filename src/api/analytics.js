81c0f2c8afd6bb184be7a52a83dc17332d47a76a
Nikhil0327 <vemulanikhil27@gmail.com>
2026-05-08 03:47:14 +0530

    Delete src directory

--- src/api/analytics.js
+++
@@ -1,9 +0,0 @@
-import api from "./client.js";
-
-export async function getSummary(year) {
-  const { data } = await api.get("/api/analytics/summary", {
-    params: { year },
-  });
-
-  return data;
-}
--- src/api/meta.js
+++
@@ -1,6 +0,0 @@
-import api from "./client.js";
-
-export async function getRegistrationMeta() {
-  const { data } = await api.get("/api/public/registration-meta");
-  return data;
-}
