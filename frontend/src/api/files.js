ab00247732ca972a8750cb15786d36c5719e5496
Nikhil0327 <vemulanikhil27@gmail.com>
2026-05-07 23:29:36 +0530

    Update file upload API endpoint to '/api/files'

--- frontend/src/api/files.js
+++ frontend/src/api/files.js
@@ -1,10 +1,16 @@
 import api from "./client.js";
 
 export async function uploadFile(file) {
+
   const form = new FormData();
+
   form.append("file", file);
-  const { data } = await api.post("/files", form, {
-    headers: { "Content-Type": "multipart/form-data" },
+
+  const { data } = await api.post("/api/files", form, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
   });
+
   return data;
 }
